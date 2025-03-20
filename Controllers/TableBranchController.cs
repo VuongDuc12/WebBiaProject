2ab77113e0b8f2a6fe76a684e47be9379becba71
VuongDuc12 <vuongduc226@gmail.com>
2025-03-20 15:44:00 +0700

    update page TableBranch

--- Controllers/TableBranchController.cs
+++ Controllers/TableBranchController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBiaProject.Data;
 using WebBiaProject.Models;
 using WebBiaProject.ViewModel;
 
@@ -6,9 +8,32 @@ namespace WebBiaProject.Controllers
 {
     public class TableBranchController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<TableBranchController> _logger;
+        private readonly ApplicationDbContext _context;
+        public TableBranchController(ApplicationDbContext context, ILogger<TableBranchController> logger)
         {
-            return View();
+            _context = context;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index(int x)
+        {
+            var branch = await _context.Branches.FindAsync(x);
+            var result = _context.BilliardTables
+                .Include(b => b.Status)
+                .Where(b => b.BranchId == x).ToList();
+
+            if (branch == null)
+            {
+                return NotFound(); // Trả về lỗi 404 nếu không tìm thấy chi nhánh
+            }
+
+            var tableBranchView = new TableBranchViewModel
+            {
+                Branch = branch,
+                billiardTables = result
+            };
+
+            return View(tableBranchView);
         }
         public IActionResult DatNgay(int tableId, int branchId)
         {
---
+++ ViewModel/TableBranchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WebBiaProject.Models;
+
+namespace WebBiaProject.ViewModel
+{
+    public class TableBranchViewModel
+    {
+        public Branch Branch { get; set; }
+        public List<BilliardTable> billiardTables { get; set; }
+
+        
+    }
+}
