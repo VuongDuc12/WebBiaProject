4ce28cbc46a98d8eea874a85664fd5a7bc1a4541
VuongDuc12 <vuongduc226@gmail.com>
2025-03-21 18:27:18 +0700

    update phân quyền người dùng bắt đăng nhập trước khi sử dụng các dịch vụ

--- Controllers/BookingPageController.cs
+++ Controllers/BookingPageController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebBiaProject.Data;
 
 namespace WebBiaProject.Controllers
 {
+    [Authorize(Roles = "Customer")]
     public class BookingPageController : Controller
     {
        
